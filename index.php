b941aff0bdf5e311d4b71ee7ea0b4927e3ada8e4
Dragan Gajic <draaagaaan@gmail.com>
2018-02-04 22:31:20 +0100

    Added get_header and get_footer

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-//Silence is golden
+<?php
+/**
+ * Themes index file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package dg
+ */
+get_header(); ?>
+
+
+<h1>Testing</h1>
+
+
+<?php
+get_sidebar();
+get_footer();
